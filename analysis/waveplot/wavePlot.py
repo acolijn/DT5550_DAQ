4a9fdb6b154ea1453867b1a5c2529c2337e44c39
acolijn <auke.pieter.colijn@gmail.com>
2021-12-23 11:47:36 +0100

    v0.0.0 of waveform plotting GUI

---
+++ analysis/waveplot/wavePlot.py
@@ -0,0 +1,89 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+sys.path.insert(0,'../python/')
+from DT5550_Waveform import *
+import wave_gui
+import glob
+N_DETECTOR = 8
+
+class WavePlotter(QMainWindow, wave_gui.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(WavePlotter, self).__init__(parent)
+        self.setupUi(self)
+        self.plotIt.clicked.connect(self.buttonClicked)
+
+        filename = "../../../data/20211217_171057/20211217_171057/waveform_20211217_171057_0.raw"
+        self.waves = DT5550_Waveform(file=filename)
+        # read first event and plot
+        self.waves.read_event()
+        self.drawPlot()
+
+    def buttonClicked(self):
+        self.waves.read_event()
+        self.drawPlot()
+
+    def drawPlot(self):
+        for i in range(5):
+            self.plotWidget.canvas.ax[i].clear()
+
+        axs = self.plotWidget.canvas.ax
+
+        # plot single event
+        imin = 0
+        imax = 1022
+
+        Q = np.zeros([N_DETECTOR])
+        Pk = np.zeros([N_DETECTOR])
+
+        plot_range=[0,1022]
+        for idet in range(N_DETECTOR):
+            #
+            # integrate waveform
+            #
+            Q[idet] = self.waves.integrate_waveform(idet)
+            Pk[idet] = self.waves.get_peak(idet)
+
+            Ratio = 0
+            if Q[idet] != 0:
+                Ratio = Pk[idet] / Q[idet]
+
+            # txt = 'CH '+str(idet)+' Q='+str(Q[idet])+' Pk='+str(Pk[idet])+' Ratio = '+str((Ratio))
+            txt = 'CH {:1d} Q = {:>5.1f} Pk = {:>5.1f} Ratio = {:>5.2f}'.format(idet, Q[idet], Pk[idet], Ratio)
+            print(txt)
+            axs[0].plot(self.waves.analog[idet][imin:imax] -
+                        self.waves.config["detector_settings"][idet]["BASE"], label=txt, drawstyle='steps')
+            axs[0].set_xlim(plot_range)
+            for idig in range(N_DIGITAL_OUT):
+                axs[1 + idig].plot(self.waves.digital[idig][idet][imin:imax], drawstyle='steps')
+                axs[1 + idig].set_xlim(plot_range)
+
+        # plot channel #8 with trigger info as well
+        for idig in range(N_DIGITAL_OUT):
+            axs[1 + idig].plot(self.waves.digital[idig][8][imin:imax], drawstyle='steps')
+            axs[1 + idig].set_xlim(plot_range)
+
+        axs[0].legend(loc='upper right')
+        # if N_DETECTOR == 8:
+        #    for idig in range(N_DIGITAL_OUT):
+        #        axs[1+idig].plot(self.digital[idig][7][imin:imax])
+        axs[0].set_ylabel('Analog (ADC)')
+        # axs[0].set_ylim([-20,20])
+        secax = axs[0].secondary_yaxis('right', functions=(self.waves.adc2v, self.waves.v2adc))
+        secax.set_color('green')
+        secax.set_ylabel('Analog (V)')
+
+        for i in range(1, 5):
+            axs[i].set_ylim([-0.1, 1.1])
+            txt = 'D' + str(i - 1)
+            axs[i].set_ylabel(txt)
+
+        self.plotWidget.canvas.draw()
+
+def main():
+    app = QApplication(sys.argv)
+    form = WavePlotter()
+    form.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
---
+++ analysis/waveplot/wave_gui.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '.\wave_gui.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1370, 1439)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.plotWidget = MplWidget(self.centralwidget)
+        self.plotWidget.setEnabled(True)
+        self.plotWidget.setGeometry(QtCore.QRect(280, 50, 1011, 1291))
+        self.plotWidget.setObjectName("plotWidget")
+        self.plotIt = QtWidgets.QPushButton(self.centralwidget)
+        self.plotIt.setGeometry(QtCore.QRect(50, 50, 150, 46))
+        self.plotIt.setObjectName("plotIt")
+        self.line = QtWidgets.QFrame(self.centralwidget)
+        self.line.setGeometry(QtCore.QRect(250, 40, 20, 1321))
+        self.line.setFrameShape(QtWidgets.QFrame.VLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line.setObjectName("line")
+        self.splitter_2 = QtWidgets.QSplitter(self.centralwidget)
+        self.splitter_2.setGeometry(QtCore.QRect(60, 170, 133, 481))
+        self.splitter_2.setOrientation(QtCore.Qt.Vertical)
+        self.splitter_2.setObjectName("splitter_2")
+        self.splitter = QtWidgets.QSplitter(self.splitter_2)
+        self.splitter.setOrientation(QtCore.Qt.Vertical)
+        self.splitter.setObjectName("splitter")
+        self.selectALL = QtWidgets.QCheckBox(self.splitter)
+        self.selectALL.setEnabled(True)
+        self.selectALL.setChecked(True)
+        self.selectALL.setObjectName("selectALL")
+        self.channel_0 = QtWidgets.QCheckBox(self.splitter)
+        self.channel_0.setObjectName("channel_0")
+        self.channel_1 = QtWidgets.QCheckBox(self.splitter)
+        self.channel_1.setObjectName("channel_1")
+        self.channel_2 = QtWidgets.QCheckBox(self.splitter)
+        self.channel_2.setObjectName("channel_2")
+        self.channel_3 = QtWidgets.QCheckBox(self.splitter_2)
+        self.channel_3.setObjectName("channel_3")
+        self.channel_5 = QtWidgets.QCheckBox(self.splitter_2)
+        self.channel_5.setObjectName("channel_5")
+        self.channel_6 = QtWidgets.QCheckBox(self.splitter_2)
+        self.channel_6.setObjectName("channel_6")
+        self.channel_7 = QtWidgets.QCheckBox(self.splitter_2)
+        self.channel_7.setObjectName("channel_7")
+        self.channel_8 = QtWidgets.QCheckBox(self.splitter_2)
+        self.channel_8.setObjectName("channel_8")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1370, 38))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Waveform Display"))
+        self.plotIt.setText(_translate("MainWindow", "Plot"))
+        self.selectALL.setText(_translate("MainWindow", "ALL"))
+        self.channel_0.setText(_translate("MainWindow", "channel 0"))
+        self.channel_1.setText(_translate("MainWindow", "channel 1"))
+        self.channel_2.setText(_translate("MainWindow", "channel 2"))
+        self.channel_3.setText(_translate("MainWindow", "channel 3"))
+        self.channel_5.setText(_translate("MainWindow", "channel 4"))
+        self.channel_6.setText(_translate("MainWindow", "channel 5"))
+        self.channel_7.setText(_translate("MainWindow", "channel 6"))
+        self.channel_8.setText(_translate("MainWindow", "channel 7"))
+
+from mplwidget import MplWidget
