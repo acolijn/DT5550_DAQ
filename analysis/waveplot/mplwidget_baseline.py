0b9c7b586dea3ece0f1d7f890666cc7fdab1cbdf
acolijn <auke.pieter.colijn@gmail.com>
2021-12-28 17:01:43 +0100

    added baseline plotter in GUI

---
+++ analysis/waveplot/mplwidget_baseline.py
@@ -0,0 +1,45 @@
+from PyQt5 import QtWidgets
+
+import matplotlib
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as Canvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+
+# Ensure using PyQt5 backend
+matplotlib.use('QT5Agg')
+
+
+class MplCanvas_Baseline(Canvas):
+    """
+    Matplotlib class to enable plotting
+    """
+    def __init__(self):
+        self.fig = Figure()
+        self.fig.subplots_adjust(
+            top=0.984,
+            bottom=0.029,
+            left=0.036,
+            right=0.98,
+            hspace=0.15,
+            wspace=0.15
+        )
+        self.ax = self.fig.subplots(4, 2)
+
+        Canvas.__init__(self, self.fig)
+        Canvas.setSizePolicy(self, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        Canvas.updateGeometry(self)
+
+
+class MplWidget_Baseline(QtWidgets.QWidget):
+    """
+    Matplotlib widget -> upgrades a standard QWidget to MplWidget -> allows plotting
+    """
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)   # Inherit from QWidget
+        self.canvas = MplCanvas_Baseline()                  # Create canvas object
+        self.vbl = QtWidgets.QVBoxLayout()         # Set box for plotting
+        self.vbl.addWidget(self.canvas)
+        self.setLayout(self.vbl)
+
+        self.toolbar = NavigationToolbar(self.canvas, self)
+        self.vbl.addWidget(self.toolbar)
--- analysis/waveplot/wavePlot.py
+++ analysis/waveplot/wavePlot.py
@@ -3,6 +3,7 @@ from PyQt5 import QtWidgets
 
 from analysis.python.DT5550_Waveform import DT5550_Waveform
 import sys
+import glob
 import wave_gui
 import numpy as np
 from matplotlib.pyplot import cm
@@ -28,6 +29,8 @@ class WavePlotter(QMainWindow, wave_gui.Ui_MainWindow):
         for i in range(len(self.checkers)):
             self.checkers[i].setChecked(True)
             self.checkers[i].stateChanged.connect(self.selectChannel)
+        self.trigger_sel.stateChanged.connect(self.selectChannel)
+        self.baselineSubtract.stateChanged.connect(self.selectChannel)
 
         self.selectALL.stateChanged.connect(self.selectAllChannels)
 
@@ -42,11 +45,23 @@ class WavePlotter(QMainWindow, wave_gui.Ui_MainWindow):
         self.actionOpenDir.triggered.connect(self.selectDataDir)
         self.actionOpenFile.triggered.connect(self.selectDataFile)
 
+        self.tabWidget.currentChanged.connect(self.tabPlot)
+
+    def tabPlot(self):
+
+        if self.tab.isVisible():
+            self.drawPlot()
+        elif self.tab_2.isVisible():
+            self.drawBaseline()
+
     def exitAction(self):
         sys.exit(0)
 
     def selectDataFile(self):
         self.filename = QtWidgets.QFileDialog.getOpenFileName(self, 'Select input waveform', r'C:\\Users\aukep\surfdrive\FineStructure\data')[0]
+        if self.filename == '':
+            return
+
         self.waves = DT5550_Waveform(file=self.filename)
         self.readAndDraw()
 
@@ -55,6 +70,10 @@ class WavePlotter(QMainWindow, wave_gui.Ui_MainWindow):
     def selectDataDir(self):
 
         self.folderpath = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select input folder', r'C:\\Users\aukep\surfdrive\FineStructure\data')
+        fnames = glob.glob(self.folderpath + r'\wave*.raw')
+        if len(fnames) == 0:
+            return
+
         self.waves = DT5550_Waveform(indir=self.folderpath)
         self.readAndDraw()
 
@@ -65,6 +84,8 @@ class WavePlotter(QMainWindow, wave_gui.Ui_MainWindow):
         self.plotIt.setEnabled(status)
         self.selectALL.setEnabled(status)
         [ch.setEnabled(status) for ch in self.checkers]
+        self.trigger_sel.setEnabled(status)
+        self.baselineSubtract.setEnabled(status)
 
     def selectAllChannels(self):
         # we will change the individual check boxes and I dont want to re-draw the picture 8x
@@ -72,20 +93,53 @@ class WavePlotter(QMainWindow, wave_gui.Ui_MainWindow):
 
         value = self.selectALL.isChecked()
         [ch.setChecked(value) for ch in self.checkers]
+        self.trigger_sel.setChecked(value)
 
         self.drawPlot()
+        self.drawBaseline()
+
         self.draw_hold = False
+
 
     def selectChannel(self):
         if not self.draw_hold:
             self.drawPlot()
+            self.drawBaseline()
+
 
     def readAndDraw(self):
         # read the next event
         if self.waves.read_event() == -1:
             self.plotIt.setEnabled(False)
 
-        self.drawPlot()
+        if self.tab.isVisible():
+            self.drawPlot()
+        elif self.tab_2.isVisible():
+            self.drawBaseline()
+
+
+    def drawBaseline(self):
+        """
+        Draw the baselines
+        """
+
+        axs = self.plotBaselineWidget.canvas.ax
+        nrow, ncol = np.shape(axs)
+
+        for irow in range(nrow):
+            for icol in range(ncol):
+                self.plotBaselineWidget.canvas.ax[irow, icol].clear()
+
+        for irow in range(nrow):
+            for icol in range(ncol):
+                idet = irow*2+icol
+                vals = self.waves.analog[idet]*self.waves.digital[2, idet]
+                vals = vals[vals > 0]
+
+                axs[irow, icol].hist(vals, bins=100, range=(0, 1000))
+
+        self.plotBaselineWidget.canvas.draw()
+
 
     def drawPlot(self):
         """
@@ -124,8 +178,13 @@ class WavePlotter(QMainWindow, wave_gui.Ui_MainWindow):
             # txt = 'CH '+str(idet)+' Q='+str(Q[idet])+' Pk='+str(Pk[idet])+' Ratio = '+str((Ratio))
             txt = 'CH {:1d} Q = {:>5.1f} Pk = {:>5.1f} Ratio = {:>5.2f}'.format(idet, Q[idet], Pk[idet], Ratio)
             if self.checkers[idet].isChecked() == True:
-                axs[0].plot(self.waves.analog[idet][imin:imax] -
-                            self.waves.config["detector_settings"][idet]["BASE"], label=txt, drawstyle='steps', c=col)
+                if self.baselineSubtract.isChecked():
+                    axs[0].plot(self.waves.analog[idet][imin:imax] -
+                                self.waves.config["detector_settings"][idet]["BASE"],
+                                label=txt, drawstyle='steps', c=col)
+                else:
+                    axs[0].plot(self.waves.analog[idet][imin:imax], label=txt, drawstyle='steps', c=col)
+
                 axs[0].set_xlim(plot_range)
                 nplot = nplot + 1
                 for idig in range(N_DIGITAL_OUT):
@@ -133,9 +192,10 @@ class WavePlotter(QMainWindow, wave_gui.Ui_MainWindow):
                     axs[1 + idig].set_xlim(plot_range)
 
         # plot channel #8 with trigger info as well
-        for idig in range(N_DIGITAL_OUT):
-            axs[1 + idig].plot(self.waves.digital[idig][8][imin:imax], ':', drawstyle='steps', c='black')
-            axs[1 + idig].set_xlim(plot_range)
+        if self.trigger_sel.isChecked():
+            for idig in range(N_DIGITAL_OUT):
+                axs[1 + idig].plot(self.waves.digital[idig][8][imin:imax], ':', drawstyle='steps', c='black')
+                axs[1 + idig].set_xlim(plot_range)
 
         if nplot >0:
             axs[0].legend(loc='upper right', fontsize=14)
